e7d9a00fc301d0503ec092074fc7c1c6bfe425ce
Anton <genesiz1990@gmail.com>
2023-10-23 19:47:00 +0300

    first

---
+++ src/main/java/ru/numismatist/PreciousMetalCoins/PreciousMetalCoinsApplication.java
@@ -0,0 +1,13 @@
+package ru.numismatist.PreciousMetalCoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreciousMetalCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreciousMetalCoinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/numismatist/PreciousMetalCoins/PreciousMetalCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.numismatist.PreciousMetalCoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreciousMetalCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

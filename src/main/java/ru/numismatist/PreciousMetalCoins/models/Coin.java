a65098fe7ec082d87dd9220b531c410f3cffce46
Anton <genesiz1990@gmail.com>
2023-10-23 20:40:42 +0300

    add db entity

---
+++ src/main/java/ru/numismatist/PreciousMetalCoins/models/Coin.java
@@ -0,0 +1,114 @@
+package ru.numismatist.PreciousMetalCoins.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Coins")
+public class Coin {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
+    @Column(name = "name")
+    private String name;
+
+    @NotEmpty(message = "Номинал не должен быть пустым")
+    @Positive
+    @Column(name = "denomination")
+    private int denomination;
+
+    @NotEmpty(message = "Металл не должен быть пустым")
+    @Size(min = 2, max = 100, message = "Название металла должно быть от 2 до 100 символов")
+    @Column(name = "metal")
+    private String metal;
+
+    @NotEmpty(message = "Вес не должен быть пустым")
+    @Positive
+    @Column(name = "weight")
+    private double weight;
+
+    @NotEmpty(message = "Каталожный номер не должен быть пустым")
+    @Size(min = 2, max = 100, message = "Каталожный номер должен быть от 2 до 100 символов")
+    @Column(name = "catalogNumber")
+    private String catalogNumber;
+
+    @NotEmpty(message = "Цена должна быть указана")
+    @Positive
+    @Column(name = "cost")
+    private int cost;
+
+    public Coin(String name, int denomination, String metal, double weight, String catalogNumber, int cost) {
+        this.name = name;
+        this.denomination = denomination;
+        this.metal = metal;
+        this.weight = weight;
+        this.catalogNumber = catalogNumber;
+        this.cost = cost;
+    }
+
+    public Coin() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(int denomination) {
+        this.denomination = denomination;
+    }
+
+    public String getMetal() {
+        return metal;
+    }
+
+    public void setMetal(String metal) {
+        this.metal = metal;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getCatalogNumber() {
+        return catalogNumber;
+    }
+
+    public void setCatalogNumber(String catalogNumber) {
+        this.catalogNumber = catalogNumber;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
